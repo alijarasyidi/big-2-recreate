25d40af98312ae21a1afa8650916ae630d823f35
alija <alijakoe@gmail.com>
2023-09-28 22:08:16 +0700

    setup Game scene and its DI lifetime scope

---
+++ Assets/Scripts/System/GameSceneLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Alija.Big2.Client
+{
+    public class GameSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
